3abf9823d425eca49c00215a6bf841c4753ded36
David Kunzmann <davidkunzmann@gmail.com>
2016-05-25 18:36:06 +0200

    Plotting the LV

--- linguistic_variable.py
+++ linguistic_variable.py
@@ -114,6 +114,7 @@ class LinguisticVariableT2:
     def plot(self):
         for name, mf in self.membership_functions.iteritems():
             mf[0].plot(self.input_values, name=name)
+            mf[1].plot(self.input_values)
             if self.input_value:
                 activation = mf(self.input_value)
                 pl.plot([self.y_min, self.input_value, self.input_value, self.input_value], [activation, activation, 0, activation], 'k--')
@@ -170,6 +171,7 @@ class FreeShapeMF:
         pl.grid(True)
 
 
-temperature = LinguisticVariableT2('temperature', 0, 35, [[[(0,1),(17,1),(20,0)],[(0,0.8),(15, 0.5),(17,0)]],[[(17,0),(20,1),(25,1), (27,0)],[(19,0),(21, 0.5),(25,0.5), (26, 0)]], [[(26,0),(28,1),(30,1)],[(19,0),(21, 0.5),(25,0.5)]]], ['cold', 'warm', 'hot'], res=1.0)
+temperature = LinguisticVariableT2('temperature', 0, 35, [[[(0,1),(17,1),(20,0)],[(0,0.8),(15, 0.8),(17,0)]],[[(17,0),(20,1),(25,1), (27,0)],[(19,0),(21, 0.5),(25,0.5), (26, 0)]], [[(26,0),(28,1),(30,1)],[(27,0),(29, 0.8),(30,0.8)]]], ['cold', 'warm', 'hot'], res=1.0)
 
 temperature.plot()
+pl.show()
